be38a6bf82e2aeb7eecd5e98eaf4201ad350e19f
Berke <runjeberke@gmail.com>
2021-02-21 20:55:05 +0300

    default reset

--- application/views/blankView.php
+++ application/views/blankView.php
@@ -8,21 +8,11 @@
 <body>
   <?php $this->load->view('includes/sidebar') ?>
 
-naber erdoo
-gsdhdsghsdfh
-sdfhdfshfsdhsfdh
-sdfhsfdhdfs<?php
-dsfgsfdgf?>
-<<<<<<< HEAD
-sadfsadfsdafsaddsa
 
-  <?php $this->load->view('includes/footer') ?>asdf
-=======
-asdfdsafsadsad
-asdasdasdasdas
+
+
   <?php $this->load->view('includes/footer') ?>
->>>>>>> master
 </body>
-asdsadasd
+
 
 </html>
